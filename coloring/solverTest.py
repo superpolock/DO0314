0330cfa9790e0f2d7e911e92e2cbc436b6ae61a3
Super Polock <superpolock@github.com>
2015-03-21 17:54:59 -0500

    Confirm correct colorset

--- coloring/solverTest.py
+++ coloring/solverTest.py
@@ -12,5 +12,22 @@ class Samples(unittest.TestCase):
         self.assertTrue(edges[1],(1,2))
         self.assertTrue(edges[2],(1,3))
 
+    def testSolveIt(self):
+        results = solver.solve_it(self.simple)
+        lines = results.splitlines()
+# first line is the number of colors
+        self.assertEqual(lines[0],"2 1")
+# and the second object was whether or not solution is optimal
+        colors = set()
+        for color in lines[1].split():
+            colors.add(int(color))
+        self.assertEqual(len(colors),2)
+        self.assertTrue(1 in colors)
+        self.assertTrue(0 in colors)
+        self.assertTrue(2 not in colors)
+        self.assertTrue(3 not in colors)
+
+
+
 if __name__ == "__main__":
         unittest.main()   
